71a2ffc4da8109001f90a6bd2e7cdd77705a9020
Aaryan Jha <98845228+saanrn@users.noreply.github.com>
2022-02-02 01:09:42 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,563 @@
+< !DOCTYPE html >
+    <
+    html lang = "en" >
+    <
+    head >
+    <
+    meta charset = "utf-8" >
+    <
+    meta http - equiv = "X-UA-Compatible"
+content = "IE=edge" >
+    <
+    title > | Khullakitab < /title> <
+    base href = "http://www.khullakitab.com/" / >
+    <!-- Mobile viewport optimized -->
+    <
+    meta name = "viewport"
+content = "width=device-width, initial-scale=1.0" >
+    <
+    meta name = "description"
+content = "" >
+    <
+    meta name = "author"
+content = "" >
+    <
+    link rel = "shortcut icon"
+href = "images/ico/favicon.ico" >
+    <!-- Latest compiled and minified bootstrap CSS -->
+    <
+    link rel = "stylesheet"
+href = "media/bootstrap/css/bootstrap.min.css" >
+
+    <!-- Optional bootstrap theme -->
+    <
+    link rel = "stylesheet"
+href = "media/bootstrap/css/bootstrap-theme.min.css" >
+
+    <!-- Optional bootstrap theme -->
+    <
+    link rel = "stylesheet"
+href = "css/animate.min.css" >
+
+    <!-- jquery ui css -->
+    <!--link rel="stylesheet" href="media/jquery-ui/jquery-ui.css"-->
+
+    <!-- FontAwesome -->
+    <
+    link rel = "stylesheet"
+href = "media/font-awesome/css/font-awesome.min.css" >
+
+    <!-- Custom CSS -->
+    <
+    link rel = "stylesheet"
+href = "css/google-fonts.css" >
+
+    <!-- responsive CSS -->
+    <
+    link rel = "stylesheet"
+href = "css/responsive.css" >
+
+    <!-- Custom CSS -->
+    <
+    link rel = "stylesheet"
+href = "css/style.css" >
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <
+    script src = "./js/html5shiv.min.js" > < /script> <
+    script src = "./js/respond.min.js" > < /script> <
+    ![endif] -->
+    <
+    script async src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" > < /script> <
+    script >
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-4184497916927519",
+        enable_page_level_ads: true
+    }); <
+/script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<
+script async src = "https://www.googletagmanager.com/gtag/js?id=UA-59042776-4" > < /script> <
+    script >
+    window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-59042776-4'); <
+/script>
+
+<
+/head>
+
+<
+body >
+    <
+    div class = "google_ads" >
+    <
+    hr >
+    <
+    div class = "container" >
+    <!-- Responsive_1 -->
+    <
+    ins class = "adsbygoogle"
+style = "display:block;width:100%;height:auto;"
+data - ad - client = "ca-pub-1243455971719754"
+data - ad - slot = "7479056825"
+data - ad - format = "auto" >
+    <
+    /ins> <
+    script >
+    (adsbygoogle = window.adsbygoogle || []).push({}); <
+/script> <
+/div> <
+hr >
+    <
+    /div> <
+    div class = "container"
+id = "top-border" >
+
+    <
+    div class = "row" >
+    <
+    div class = "col-sm-4 border-shadow" > < /div> <
+    div class = "col-sm-4 col-sm-offset-4 border-shadow" > < /div> <
+    /div> <
+    /div> <
+    header id = "header" >
+    <
+    div class = "container" >
+    <
+    div class = "row" >
+    <
+    div class = "col-sm-6 col-xs-4 hidden-lg hidden-md" >
+    <
+    a class = "navbar-brand"
+href = "./" > < img src = "images/logo.png"
+alt = "Khulla Kitab Logo"
+width = "50" > Khullakitab.com < /a> <
+    /div>
+
+    <
+    div class = "col-sm-6 col-xs-8 pull-right" >
+    <
+    div class = "social" >
+    <
+    ul class = "social-share" >
+    <
+    li > < a href = "https://www.facebook.com/khullakitab" > < i class = "fa fa-facebook" > < /i></a > < /li> <
+    li > < a href = "https://www.facebook.com/khullakitab" > < i class = "fa fa-twitter" > < /i></a > < /li>
+    <!--li><a href="https://www.facebook.com/khullakitab"><i class="fa fa-linkedin"></i></a></li>
+    <
+    li > < a href = "https://www.facebook.com/khullakitab" > < i class = "fa fa-dribbble" > < /i></a > < /li> <
+    li > < a href = "https://www.facebook.com/khullakitab" > < i class = "fa fa-skype" > < /i></a > < /li--> <
+    /ul> <
+    /div> <
+    /div> <
+    /div><!-- end .row --> <
+    /div><!--end .container--> <
+    nav class = "navbar" >
+    <
+    div class = "container" >
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <
+    div class = "navbar-header" >
+    <
+    button type = "button"
+class = "navbar-toggle"
+data - toggle = "collapse"
+data - target = ".navbar-collapse" >
+    <
+    span class = "sr-only" > Toggle navigation < /span> <
+    span class = "icon-bar" > < /span> <
+    span class = "icon-bar" > < /span> <
+    span class = "icon-bar" > < /span> <
+    /button> <
+    /div><!-- end .navbar-header -->
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <
+    div class = "collapse navbar-collapse"
+id = "bs-example-navbar-collapse-1" >
+    <
+    ul class = "nav navbar-nav" >
+    <
+    li class = "dropdown" >
+    <
+    a href = "#"
+class = "dropdown-toggle"
+data - toggle = "dropdown" > NOTES < /a> <
+    ul class = "dropdown-menu" >
+    <
+    li > < a href = "class-8/subjects/notes" > Class 8 < /a></li >
+    <
+    li > < a href = "class-9/subjects/notes" > Class 9 < /a></li >
+    <
+    li > < a href = "class-10/subjects/notes" > Class 10 < /a></li >
+    <
+    li > < a href = "grade-11/subjects/notes" > Grade 11 < /a></li >
+    <
+    li > < a href = "grade-12/subjects/notes" > Grade 12 < /a></li >
+    <
+    /ul> <
+    /li> <
+    li class = "dropdown" >
+    <
+    a href = "#"
+class = "dropdown-toggle"
+data - toggle = "dropdown" > SOLUTIONS < /a> <
+    ul class = "dropdown-menu" >
+    <
+    li > < a href = "class-8/subjects/solutions" > Class 8 < /a></li >
+    <
+    li > < a href = "class-9/subjects/solutions" > Class 9 < /a></li >
+    <
+    li > < a href = "class-10/subjects/solutions" > Class 10 < /a></li >
+    <
+    li > < a href = "grade-11/subjects/solutions" > Grade 11 < /a></li >
+    <
+    li > < a href = "grade-12/subjects/solutions" > Grade 12 < /a></li >
+    <
+    /ul> <
+    /li> <
+    li class = "dropdown" >
+    <
+    a href = "#"
+class = "dropdown-toggle"
+data - toggle = "dropdown" > PASTPAPER < /a> <
+    ul class = "dropdown-menu" >
+    <
+    li > < a href = "class-8/subjects/pastpapers" > Class 8 < /a></li >
+    <
+    li > < a href = "class-9/subjects/pastpapers" > Class 9 < /a></li >
+    <
+    li > < a href = "class-10/subjects/pastpapers" > Class 10 < /a></li >
+    <
+    li > < a href = "grade-11/subjects/pastpapers" > Grade 11 < /a></li >
+    <
+    li > < a href = "grade-12/subjects/pastpapers" > Grade 12 < /a></li >
+    <
+    /ul> <
+    /li> <
+    li class = "dropdown" >
+    <
+    a href = "#"
+class = "dropdown-toggle"
+data - toggle = "dropdown" > PRACTICE < /a> <
+    ul class = "dropdown-menu" >
+    <
+    li > < a href = "class-8/subjects/practices" > Class 8 < /a></li >
+    <
+    li > < a href = "class-9/subjects/practices" > Class 9 < /a></li >
+    <
+    li > < a href = "class-10/subjects/practices" > Class 10 < /a></li >
+    <
+    li > < a href = "grade-11/subjects/practices" > Grade 11 < /a></li >
+    <
+    li > < a href = "grade-12/subjects/practices" > Grade 12 < /a></li >
+    <
+    /ul> <
+    /li> <
+    /ul> <
+    div class = "navbar-header navbar-right col-md-3" >
+    <!-- google custom search -->
+    <
+    script >
+    (function() {
+        var cx = '014286562503469598260:lft-vqow4ba';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+            '//cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+    })(); <
+/script> <
+gcse: search > < /gcse:search> <
+    /div>
+    <!--ul class="nav navbar-nav navbar-right"><li><a href="./forum/" >Forum</a></li></ul-->
+
+    <
+    /div><!-- /.navbar - collapse -->
+    <
+    /div><!--/.container-- >
+    <
+    /nav><!--/nav-- >
+    <
+    div class = "container logo hidden-sm hidden-xs" >
+    <
+    a href = "http://www.khullakitab.com/" > < img src = "images/logo.png"
+alt = "Khulla kitab logo"
+title = "Khull Kitab" > < /a> <
+    /div><!--/.container-- >
+    <
+    /header><!--end header-->
+
+    <
+    div class = "background" > < /div> <
+    section id = "single-page"
+class = "no-margin" >
+    <
+    div class = "container" >
+    <
+    h1 > Note Found! < /h1> <
+    div >
+    <
+    /div> <
+    /div> <
+    /section> <
+    div class = "google_ads" >
+    <
+    hr >
+    <
+    div class = "container" >
+    <!-- Responsive_1 -->
+    <
+    ins class = "adsbygoogle"
+style = "display:block;width:100%;height:auto;"
+data - ad - client = "ca-pub-1243455971719754"
+data - ad - slot = "7479056825"
+data - ad - format = "auto" >
+    <
+    /ins> <
+    script >
+    (adsbygoogle = window.adsbygoogle || []).push({}); <
+/script> <
+/div> <
+hr >
+    <
+    /div> <
+    footer id = "footer" >
+    <
+    div class = "container" >
+    <
+    div class = "row" >
+    <
+    div class = "col-xs-6 col-sm-6 col-md-4" >
+    <
+    div class = "footer_menu" >
+    <
+    ul >
+    <
+    li > < a href = "http://www.khullakitab.com/" > Home < /a> </li >
+    <
+    li > < a href = "?type=notes#middle" > Notes < /a> </li >
+    <
+    li > < a href = "?type=solutions#middle" > Solutions < /a> </li >
+    <
+    li > < a href = "?type=pastpapers#middle" > Pastpaper < /a> </li >
+    <
+    li > < a href = "?type=#middle" > Practice Papers < /a> </li >
+    <
+    /ul> <
+    /div> <
+    /div> <
+    div class = "col-xs-6 col-sm-6 col-md-4" >
+    <
+    div class = "footer_menu" >
+    <
+    ul >
+    <
+    li > < a href = "about" > About < /a></li >
+    <
+    li > < a href = "khullakitab-faq" > FAQ < /a></li >
+    <
+    li > < a href = "#" > BLOG < /a> </li >
+    <
+    li > < a href = "forum" > FORUM < /a> </li >
+    <
+    /ul> <
+    /div> <
+    /div> <
+    div class = "col-xs-12 col-md-4" >
+    <
+    p > & copy;
+Copyright 2014 - 2022 Khulla Kitab Edutech Pvt.Ltd. < /p>
+    <!-- facebook like page -->
+    <
+    iframe src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fkhullakitab&tabs&width=340&height=214&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId"
+width = "340"
+height = "214"
+style = "border:none;overflow:hidden"
+scrolling = "no"
+frameborder = "0"
+allowTransparency = "true" > < /iframe>
+    <!-- end facebook like page -->
+    <
+    /div> <
+    /div><!-- end .row -->
+
+    <
+    /div><!-- end .container-fluid --> <
+    /footer>
+
+    <!-- All Javascript at the bottom of the page for faster page loading -->
+    <!-- First try for the online version of jQuery-->
+    <
+    script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js" > < /script>
+
+    <!-- If no online access, fallback to our hardcoded version of jQuery -->
+    <
+    script > window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>') < /script>
+
+    <!-- Bootstrap JS -->
+    <
+    script src = "media/bootstrap/js/bootstrap.min.js" > < /script>
+
+    <!-- Custom JS -->
+    <
+    script src = "js/script.js" > < /script>
+
+    <!-- for mathjax -->
+    <!-- mathjax for offonline -->
+    <
+    script type = "text/x-mathjax-config" >
+    MathJax.Hub.Config({
+        tex2jax: {
+            inlineMath: [
+                ["$", "$"],
+                ["\\(", "\\)"]
+            ]
+        }
+    }); <
+/script> <
+script type = "text/javascript"
+src = "media/mathjax/MathJax.js?config=TeX-AMS_HTML-full" > < /script>
+
+    <!-- for google ads -->
+    <
+    script async src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" > < /script> 
+
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <
+    script type = "text/javascript"
+src = "//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54f453463e9409fa"
+async = "async" > < /script>
+
+<
+a href = "#"
+class = "scrollToTop" > < i class = "fa fa-fighter-jet" > < /i><br>Go Top</a >
+
+    <
+    div class = "modal fade"
+id = "myModal" >
+    <
+    div class = "modal-dialog" >
+    <
+    div class = "modal-content" >
+    <
+    div class = "modal-header" >
+    <
+    button class = "close"
+data - dismiss = "modal" > & times; < /button>
+
+<
+h4 class = "modal-title" > SUBSCRIBE < /h4> <
+    /div><!-- end modal-header --> <
+    div class = "modal-body" >
+    <
+    form method = "post" >
+    <
+    div class = "form-group" >
+    <
+    label
+for = "inputName" > Full Name < span > * < /span></label >
+    <
+    input type = "text"
+class = "form-control"
+id = "inputName"
+placeholder = "Type Your Full Name" >
+    <
+    /div> <
+    div class = "form-group" >
+    <
+    label
+for = "inputEmail" > Email < span > * < /span></label >
+    <
+    input type = "email"
+class = "form-control"
+id = "inputEmail"
+placeholder = "Your Email" >
+    <
+    /div> <
+    div class = "form-group" >
+    <
+    label
+for = "inputGrade" > Level < span > * < /span></label >
+    <
+    select id = "inputGrade"
+class = "form-control" >
+    <
+    option value = "2" > Class 8 < /option><option value="3">Class 9</option > < option value = "4" > Class 10 < /option><option value="5">Grade 11</option > < option value = "6" > Grade 12 < /option>                        </select >
+    <
+    /div> <
+    div class = "form-group" >
+    <
+    label
+for = "inputAcademyName" > School / College Name < /label> <
+    input type = "text"
+class = "form-control"
+id = "inputAcademyName"
+placeholder = "Type Your School / College Name" >
+    <
+    /div> <
+    button type = "submit"
+class = "btn btn-info pull-right"
+id = "subSend" > SEND! < /button> <
+    div class = "clearfix" > < /div> <
+    /form> <
+    /div><!-- end modal-body --> <
+    div class = "modal-footer" >
+    <
+    button class = "btn btn-danger"
+data - dismiss = "modal"
+type = "button" > Close < /button> <
+    /div><!-- end modal-footer --> <
+    /div><!-- end modal-content --> <
+    /div><!-- end modal-dialog --> <
+    /div><!-- end myModal --><script>
+$(function() {
+
+    $("#subSend").click(function(e) {
+
+        e.preventDefault();
+        var name = $("#inputName").val();
+        var email = $("#inputEmail").val();
+        var grade = $("#inputGrade").val();
+        var school_college_name = $("#inputAcademyName").val();
+        if (name != '' && email != '' && school_college_name != '') {
+            var params = {
+                name: name,
+                email: email,
+                grade: grade,
+                school_college_name: school_college_name
+            };
+        } else {
+            alert('Eempty field');
+            return false;
+        }
+        var request = $.ajax({
+            url: "ajax/subscribe.php",
+            type: "POST",
+            data: $.param(params),
+            dataType: 'html',
+
+        });
+        request.done(function(msg) {
+            $("#output").html(msg);
+        });
+
+        request.fail(function(jqXHR, textStatus) {
+            alert("Request failed: " + textStatus);
+        });
+    });
+}); <
+/script> <
+div id = "output" > < /div> <
+    /body> <
+    /html>
